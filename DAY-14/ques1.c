f244a73db18d6dd187691402a0201b3cfdf01b13
tanvidhuliya-2006 <tanvidhuliya@gmail.com>
2026-06-13 17:53:36 +0530

    Add files via upload

---
+++ DAY-14/ques1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int n,arr[10],item;
+    printf("Enter the number of elements");
+    scanf("%d",&n);
+    printf("Enter the array elements");
+    for(int i=0;i<n;i++)
+    {
+            scanf("%d",&arr[i]);
+        
+    }
+    printf("Enter the item to be searched");
+    scanf("%d",&item);
+        int pos=-1;
+        for(int i=0;i<n;i++)
+        {
+           if(arr[i]==item)
+           {
+            pos=i;
+            break;
+           } 
+        }
+        if(pos>=0)
+        {
+            printf("%d is found in position %d ",item,pos+1);
+        }
+        else
+        {
+            printf("%d not found",item);
+        }
+    }
